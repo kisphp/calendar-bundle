a89b58ec30b40603d4d0c4f9ba2d87152af5d273
Bogdan Rizac <mariusbogdan83@gmail.com>
2017-07-21 22:01:57 +0200

    updated calendar api

--- src/CalendarBundle/Services/Calendar.php
+++ src/CalendarBundle/Services/Calendar.php
@@ -18,6 +18,21 @@ class Calendar
     protected $days = [];
 
     /**
+     * @var \DateTime
+     */
+    protected $selectedDate;
+
+    /**
+     * @var \DateTime
+     */
+    protected $previousMonthDate;
+
+    /**
+     * @var \DateTime
+     */
+    protected $nextMonthDate;
+
+    /**
      * @param AbstractTranslation $translation
      */
     public function __construct(AbstractTranslation $translation)
@@ -34,7 +49,9 @@ class Calendar
      */
     public function generateData($year, $month, $day)
     {
-        $selectedDate = \DateTime::createFromFormat(
+        $today = new \DateTime('now');
+
+        $this->selectedDate = \DateTime::createFromFormat(
             'Y-m-d',
             sprintf(
                 '%d-%d-%d',
@@ -44,17 +61,17 @@ class Calendar
             )
         );
 
-        $currentMonth = clone $selectedDate;
+        $currentMonth = clone $this->selectedDate;
         $currentMonth->modify('last day of this month');
 
-        $previousMonthDate = (clone $selectedDate)->sub(new \DateInterval('P1M'));
-        $previousMonthDate->modify('last day of this month');
+        $this->previousMonthDate = (clone $this->selectedDate)->sub(new \DateInterval('P1M'));
+        $this->previousMonthDate->modify('last day of this month');
 
-        $nextMonthDate = (clone $selectedDate)->add(new \DateInterval('P1M'));
-        $nextMonthDate->modify('first day of this month');
+        $this->nextMonthDate = (clone $this->selectedDate)->add(new \DateInterval('P1M'));
+        $this->nextMonthDate->modify('first day of this month');
 
-        $lastDayPreviusMonthEnd = (int) $previousMonthDate->format('N');
-        $firstDayNextMonthStart = 7 - (int) $nextMonthDate->format('N');
+        $lastDayPreviusMonthEnd = (int) $this->previousMonthDate->format('N');
+        $firstDayNextMonthStart = 7 - (int) $this->nextMonthDate->format('N');
         $lastDayCurrentMonth = (int) $currentMonth->format('d');
 
         $totalDaysToShow = $lastDayPreviusMonthEnd + $lastDayCurrentMonth + $firstDayNextMonthStart + 1;
@@ -68,7 +85,7 @@ class Calendar
             $columnNumber = (int) ceil($columnIndex / 7);
             if ($columnNumber === 1 && $columnIndex <= $lastDayPreviusMonthEnd) {
                 $this->days[] = [
-                    'value' => (int) $previousMonthDate->format('d') - $lastDayPreviusMonthEnd + $columnNumber,
+                    'value' => (int) $this->previousMonthDate->format('d') - $lastDayPreviusMonthEnd + $columnNumber,
                     'class' => null,
                     'out' => true,
                 ];
@@ -78,7 +95,7 @@ class Calendar
             if ($dayNumber <= $lastDayCurrentMonth) {
                 $this->days[] = [
                     'value' => $dayNumber,
-                    'class' => ($dayNumber === (int) $selectedDate->format('d')) ? 'active' : '',
+                    'class' => $this->getClassForDay($today, $dayNumber),
                     'out' => false,
                 ];
                 $dayNumber++;
@@ -115,5 +132,58 @@ class Calendar
         }
 
         return $columns;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentDate()
+    {
+        $monthIndex = (int) $this->selectedDate->format('m');
+
+        return $this->trans->getMonthName($monthIndex);
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentYear()
+    {
+        return $this->selectedDate->format('Y');
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getNextMonth()
+    {
+        return $this->nextMonthDate;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getPreviousMonth()
+    {
+        return $this->previousMonthDate;
+    }
+
+    /**
+     * @param \DateTime $today
+     * @param $dayNumber
+     *
+     * @return string
+     */
+    protected function getClassForDay(\DateTime $today, $dayNumber)
+    {
+        if (
+            $this->selectedDate->format('Y') === $today->format('Y')
+            && $this->selectedDate->format('m') === $today->format('m')
+            && $dayNumber === (int) $this->selectedDate->format('d')
+        ) {
+            return 'active';
+        }
+
+        return '';
     }
 }
