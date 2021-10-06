db57d5ab4b616e4b4e4243a1d4a618bcee4887c2
kisphp <kisphp@users.noreply.github.com>
2021-10-06 08:02:15 +0200

    upgrade gulp task

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,14 +1,24 @@
-let gulp = require('gulp');
-let scss = require('gulp-sass');
+const { series } = require('gulp');
 
-gulp.task('scss', () => {
-    return gulp.src('gulp/assets/scss/calendar.scss')
-        .pipe(scss())
-        .pipe(gulp.dest('src/CalendarBundle/Resources/public/css'));
-});
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var clean = require('gulp-clean');
 
-gulp.task('watch', ['scss'], function(){
-    gulp.watch('gulp/assets/scss/**/*.scss', ['scss'])
-});
+const config = {
+    'source_file': 'gulp/assets/scss/calendar.scss',
+    'target_directory': 'src/CalendarBundle/Resources/public/css',
+};
 
-gulp.task('default', ['scss']);
+function cleanCss() {
+    return gulp.src(config.target_directory, {read: false})
+        .pipe(clean());
+}
+
+function buildCss() {
+    return gulp.src(config.source_file)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(config.target_directory))
+}
+
+exports.build = buildCss;
+exports.default = series(cleanCss, buildCss);
